8afd8281300521218a7090d9a336651fd4e8145e
Kishore Gopalakrishnan <kishore96@gmail.com>
2022-04-28 11:39:52 +0530

    Add more tests

--- tests.py
+++ tests.py
@@ -6,5 +6,8 @@ x,y = sy.symbols("x y")
 
 #Test simplification
 assert average(sy.sin(y)*sy.cos(y), set([])).simplify() == sy.sin(2*y)/2
+assert average(sy.sin(y), set([x])).simplify() == sy.sin(y)
 assert average(sy.sin(y)*sy.cos(y), set([x])).simplify() == sy.sin(2*y)/2
 assert average(sy.sin(y)*sy.cos(x), set([x])).simplify() == sy.sin(y)*average(sy.cos(x), set([x]))
+assert average(sy.sin(x) + sy.cos(x), set([x])).simplify() == average(sy.sin(x), set([x])) + average(sy.cos(x), set([x]))
+assert average(sy.sin(y) + sy.cos(x), set([x])).simplify() == sy.sin(y) + average(sy.cos(x), set([x]))
