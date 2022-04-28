9e9fb2c3df16c7bd6c3dbb6a47171eeb4ea72581
Kishore Gopalakrishnan <kishore96@gmail.com>
2022-04-28 12:57:47 +0530

    Add more tests

--- tests.py
+++ tests.py
@@ -4,6 +4,10 @@ from average import average
 
 x,y = sy.symbols("x y")
 
+#Test getting free symbols
+assert average(x, set([x])).free_symbols == set()
+assert average(y*x, set([x])).free_symbols == set([y])
+
 #Test simplification
 assert average(sy.sin(y)*sy.cos(y), set([])).simplify() == sy.sin(2*y)/2
 assert average(sy.sin(y), set([x])).simplify() == sy.sin(y)
